e6d6eab2ac65a4147c0f6ebba6d975a892343a94
michael <boss0456280@gmail.com>
2025-07-03 16:33:59 +0800

    重新部署資料庫異常錯誤

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { OAuth2Client } from "google-auth-library";
-import pool from "./dbconnections.js";
+import pool from "../models/dbconnections";
 
 const router = express.Router();
 
